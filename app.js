d8de6c98bdb7761787df6896db4619d1ce06c8cc
jatinder <jssingh134@gmail.com>
2024-08-21 06:56:19 +1000

    project setup and init frontend

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+//settting View engines and views
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+//set ejsMate as engine
+app.engine("ejs", ejsMate);
+
+//serve static file
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/", (req, res, next) => {
-  res.json(" app has been started welcome to the app");
+  res.render("auth/signin.ejs");
 });
 
-app.listen(3000, (err) => {
+app.listen(8080, (err) => {
   console.log(err);
   mongoose.connect("mongodb://localhost:27017/cms_db");
 });
